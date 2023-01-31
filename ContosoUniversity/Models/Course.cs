f381e382f8ab9df06129c383ecd1b8aec91931fa
revlienn <revnathalia@gmail.com>
2023-01-31 20:37:11 +0000

    Add project files.

---
+++ ContosoUniversity/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)] // lets us enter PK/CourseID instead db auto-generate
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; } // ICollection==relates to many
+    }
+}
